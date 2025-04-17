8aaa99d897cf0e6fe6cbca88f8af62198172f678
Caio Ferreira <caioferreiraadev@gmail.com>
2025-04-17 19:33:11 -0300

    Delete routes.js

--- routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import LoginController  from './src/controllers/LoginController.js'
-
-const router = express.Router();
-
-router.get("/", LoginController.index)
-
-export default router;
